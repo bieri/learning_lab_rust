53453d0128bcccb3bfdc6034d55df45b6e9c2ff5
Kevin Bieri <kevin@Kevins-MacBook-Pro.local>
2018-05-17 12:54:00 -0500

    More updates to involve looping in our guessing game and passing that info back and testing to see how panicks work in regards to the expect statement.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,35 @@
+extern crate rand;
 use std::io;
+use std::cmp::Ordering;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number?!");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed {}",guess);
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        //let guess:u32  = input.trim().parse().expect("");
+        let guess:u32   =match input.trim().parse() {
+                Ok(num) => num,
+                Err(_)=> {
+                    println!("Please input a number. Try again...");
+                            continue;}
+            }; 
+        println!("You guessed {}",input);
+        println!("The secret number is {}",secret_number);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Large"),
+            Ordering::Equal => break,
+        }    
+    }
+    
+    
+    
+     
 }
